9ae0dc7d75056ae27328d8c4aa216d42826a4082
Sebastiano Melita <sebastiano.melita@gmail.com>
2022-12-28 00:44:29 +0100

    Update Scheduler2.h

--- Scheduler2.h
+++ Scheduler2.h
@@ -4,49 +4,143 @@
 #include <Arduino.h>
 #define NEVENTS		20
 #define NTIMES		10
+#define PERIODIC 0
+#define ASYNC_A	 1
+#define ASYNC_B	 2
+
+// https://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Mediator
+//per disaccoppiare Scheduler e gli eventi è necessario far derivare entrambi 
+//da genitori che non hanno riferimenti incrociati (pattern mediatore).
+//Inoltre, per poter invocare doEvent() su eventi diversi è necessario
+//realizzare un late binding con un metodo virtuale
 
 using PEventCallback = void (*)(); //type aliasing
 #define DISABLED	128
 
-class PeriodicEvnt{
+class Sched{
 	public:
+	uint8_t id;
+};
+
+class Evnt{
+	public:
+		Sched *sch;
 		unsigned order;
 		bool enabled;
+		bool tochange;
+		uint8_t type;
+		//uint8_t pos;
+		unsigned long time;
+		unsigned long counter;
 		PEventCallback pevent;
-		PeriodicEvnt(PEventCallback, uint8_t, bool);
+		Evnt(Sched *s, unsigned long when, PEventCallback x, uint8_t priority, bool enable, uint8_t etype){
+			time = when;
+			sch = s;
+			pevent = x;
+			order = priority;
+			enabled = enable;
+			type = etype;
+			if(enabled == false){
+				order = order + DISABLED;
+			}
+			counter = 0;
+		};
+		virtual void doEvent(unsigned long step);	//late binding
+};
+
+			//late binding
+class PeriodicEvnt: public Evnt{
+	public:
+		void doEvent(unsigned long step){
+			(*pevent)();
+		};
+		PeriodicEvnt(Sched *x, unsigned long y, PEventCallback z, uint8_t u, bool v, uint8_t t):Evnt(x,y,z,u,v,t){};
+};
+
+class AsyncEvntA: public Evnt{
+	public:
+		unsigned long slaveTime;
+		uint8_t slaveOrder;
+		unsigned long duration;
+		unsigned long interval;
+		bool periodic;
+		void doEvent(unsigned long step);
+		
+		AsyncEvntA(Sched *x, unsigned long y, unsigned long z, uint8_t m, PEventCallback n, uint8_t k, bool u, uint8_t v, unsigned long howlong, unsigned long every, bool repeat):Evnt(x,y,n,k,u,v){
+			duration = howlong; 
+			interval = every; 
+			periodic = repeat; 
+			slaveTime = z;
+			slaveOrder = m;
+		};
+};
+
+class AsyncEvntB: public Evnt{
+	public:
+		unsigned long slaveTime;
+		uint8_t slaveOrder;
+		unsigned long duration;
+		unsigned long interval;
+		bool periodic;
+		void doEvent(unsigned long step);
+		
+		AsyncEvntB(Sched *x, unsigned long y, unsigned long z, uint8_t m, PEventCallback n, uint8_t k, bool u, uint8_t v, unsigned long howlong, unsigned long every, bool repeat):Evnt(x,y,n,k,u,v){
+			duration = howlong; 
+			interval = every; 
+			periodic = repeat; 
+			slaveTime = z;
+			slaveOrder = m;
+		};
+};
+
+class TCB{
+	public:
+		Evnt *events[NEVENTS];
+		//uint8_t pos;
+		uint8_t fe;// first empty
+		uint8_t enabled;
+		unsigned long step;
+		unsigned long time;
+		TCB(){step = 0; time = 0; enabled = 0; fe = 0;};
+		bool addEvent(Evnt *);
+		void sort(Evnt **, uint8_t);
+		void scambia(Evnt **, Evnt **);
+		int cerca(uint8_t, Evnt **, int);
+		bool getEventState(uint8_t order);
+		bool enableEvent(uint8_t);
+		bool disableEvent(uint8_t);
+		//bool disableEventPos(uint8_t pos);
+		//bool enableEventPos(uint8_t pos);
 };		
 
-class Scheduler{
+class Scheduler: public Sched{
 	private:
-		PeriodicEvnt *events[NTIMES][NEVENTS];
-		uint8_t ntimes;
-		uint8_t fe[NTIMES];// first empty
-		uint8_t enabled[NTIMES];
-		unsigned long steplist[NTIMES];
+		uint8_t nt;
+		TCB tasks[NTIMES];
 		unsigned long tbase;
-		unsigned tlen;
 		unsigned long prec=0;
 		volatile unsigned long step;
 		int gcd(int, int);
 		unsigned long nsteps;
-		unsigned findGCD(unsigned long [], uint8_t);
-		void sort(PeriodicEvnt **, uint8_t);
-		void scambia(PeriodicEvnt **, PeriodicEvnt **);
-		int cerca(uint8_t, PeriodicEvnt **, int);
+		unsigned findGCD();
+		void timeSort(TCB*, uint8_t);
+		int timeSearch(unsigned long, TCB*, int);
 		void maxstepCalc();
+		void setTimes();
+		int addTime( unsigned long);
 
 	public:
-		Scheduler(unsigned long [], unsigned);
-		bool addEvent(PEventCallback , uint8_t, int);
-		bool enableEvent(uint8_t, int);
-		bool disableEvent(uint8_t, int);
+		Scheduler();
+		bool addPeriodicEvent(PEventCallback pevnt, uint8_t priority, unsigned long when);
+		bool addAsyncEvent(PEventCallback pevnt, uint8_t priority, unsigned long when, unsigned long howlong, unsigned long every, bool repeat);
 		void scheduleAll();
 		unsigned getTimebase();
 		unsigned long getNsteps();
-		unsigned getTime(uint8_t);
-		void setTimes(unsigned long [], unsigned);
-		bool getEventState(uint8_t order, int nt);
-		bool setEventState(uint8_t order, bool state, int nt);
+		long getTime(unsigned long when);
+		bool getEventState(uint8_t order, unsigned long);
+		bool setEventState(uint8_t order, bool state, unsigned long);
+		bool enableEvent(uint8_t, unsigned long);
+		bool disableEvent(uint8_t, unsigned long);
 };
 
 #endif
