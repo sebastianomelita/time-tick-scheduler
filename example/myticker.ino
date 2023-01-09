f7d0f7b3fab3834d285b131d7ca6cf101625a7ea
Sebastiano Melita <sebastiano.melita@gmail.com>
2023-01-09 19:37:15 +0100

    Update myticker.ino

--- example/myticker.ino
+++ example/myticker.ino
@@ -19,20 +19,24 @@ void periodicBlink(int led) {
   digitalWrite(led, !digitalRead(led));
 }
 
-void epoch10sec(){
+void epoch10secDisable(){
 	scheduler.disableEvent(1,1000);
 	scheduler.disableEvent(1,500);
+	scheduler.enableEvent(1,10000);
+	scheduler.disableEvent(2,10000);
 	digitalWrite(led1, LOW);
 	digitalWrite(led2, LOW);
 	Serial.println("Ending timers...");
 	Serial.print("Frized... ");
 }
 
-void epoch20sec(){
+void epoch10secEnable(){
 	Serial.print("Enable all... ");
 	Serial.println(count);
 	scheduler.enableEvent(1,1000);
 	scheduler.enableEvent(1,500);
+	scheduler.disableEvent(1,10000);
+	scheduler.enableEvent(2,10000);
 	Serial.println("Starting timers...");
 }
 
@@ -49,8 +53,8 @@ void setup() {
 	Serial.begin(115200); 
 	//scheduler.addEvent(onMaxSpeedEvents, 1, 0);
 	scheduler.addPeriodicEvent(onHalfSecondEvents, 1, 500);
-	scheduler.addPeriodicEvent(epoch10sec, 1, 10000);
-	scheduler.addPeriodicEvent(epoch20sec, 1, 20000);
+	scheduler.addPeriodicEvent(epoch10secEnable, 1, 10000);
+	scheduler.addPeriodicEvent(epoch10secDisable, 2, 10000);
 	scheduler.addPeriodicEvent(onSecondEvents, 1, 1000);
 	Serial.println(F("Scheduler init"));
 	Serial.print(F("Time base: "));
