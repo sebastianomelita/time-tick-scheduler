380d6f377f35986b02b9335a45969764b9364100
Sebastiano Melita <sebastiano.melita@gmail.com>
2023-01-09 19:39:00 +0100

    Update schedAsync.ino

--- example/schedAsync.ino
+++ example/schedAsync.ino
@@ -11,7 +11,7 @@ void onSecondEvents();
 void epochScheduler();
 void epoch10sec();
 void epoch20sec();
- 
+
 void periodicBlink(int led) {
   Serial.print("printing periodic blink led ");
   Serial.println(led);
@@ -33,10 +33,11 @@ void onSecondEvents(){
 void setup() {
 	pinMode(led1, OUTPUT);
 	pinMode(led2, OUTPUT);
+	Serial.setTimeout(10);
 	Serial.begin(115200); 
+	//scheduler.addPeriodicEvent(onMaxSpeedEvents, 1, 0);
 	scheduler.addPeriodicEvent(onHalfSecondEvents, 1, 500);
-	scheduler.addAsyncEvent(epoch20sec, 1, 6000, 5000, 500, true); 	  // repeat
-	//scheduler.addAsyncEvent(epoch20sec, 2, 6000, 5000, 500, false); // no repeat
+	scheduler.addAsyncEvent(epoch20sec, 2, 6000, 5000, 500, true);
 	Serial.println(F("Scheduler init"));
 	Serial.print(F("Time base: "));
 	Serial.println(scheduler.getTimebase());
